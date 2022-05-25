647408ae8446aeb62107a96905c3ff01e61967d7
eddyhood <eddy@ignitespot.com>
2022-05-25 14:48:09 -0600

    Add bootstrap and dev server to Project

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+/* For the autoprefixer required to use bootstap with webpack.  
+
+https://github.com/postcss/autoprefixer#webpack
+
+*/
+
+module.exports = {
+    plugins: [
+      require('autoprefixer')
+    ]
+  }
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'to-goal home',
+      filename: 'index.html',
+      template: 'src/template.html',
+      inject: 'body'
+    }),
+  ],
+};
