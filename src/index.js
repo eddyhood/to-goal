26fdf20401ad79037819e06c72cb307656b0fa55
eddyhood <eddy@ignitespot.com>
2022-05-27 17:44:23 -0600

    Show form on button click
    When user clicks the add goal button in nav, display the form.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import style from './style/style.css';
 
 //Page layout
 displayLogo();
+const goalForm = document.getElementById('goal-form');
+const addBtn = document.getElementById('goal-btn');
+addBtn.addEventListener('click', e => {
+    goalForm.style.display = 'block';
+})
 
 
 
