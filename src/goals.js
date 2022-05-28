068b475bb59d8800da5992306ca6dd1ad300c332
eddyhood <eddy@ignitespot.com>
2022-05-28 11:51:12 -0600

    Add icons to goals in sidebar
    Create an edit and a delete icon. Put them in a div with the title
    header and give them a class of goal-header. Style that class as a
    flexbox and put space between so that the title and icons stay aligned.

--- src/goals.js
+++ src/goals.js
@@ -1,3 +1,11 @@
+class Goal {
+    constructor(title, description, dueDate) {
+        this.title = title;
+        this.description = description;
+        this.due = dueDate;
+    }
+}
+
 export function createNewGoal(e) {
     const title = (e.target.elements['goal-title'].value);
     const description = (e.target.elements['goal-description'].value);
@@ -6,13 +14,6 @@ export function createNewGoal(e) {
     saveGoalToLocalStorage(title, goal);
 }
 
-class Goal {
-    constructor(title, description, dueDate) {
-        this.title = title;
-        this.description = description;
-        this.due = dueDate;
-    }
-}
 
 function saveGoalToLocalStorage(key, goal){
     window.localStorage.setItem(key, JSON.stringify(goal));
@@ -24,19 +25,36 @@ export function showGoals() {
 
     for(let key of keys){
         let goal = JSON.parse(window.localStorage.getItem(key));
-        console.log(goal.title);
 
         //create li to hold goal
         const entry = document.createElement('li');
         entry.classList.add('list-group-item');
-        //title
+
+        //header (title)
+        const header = document.createElement('div');
+        header.classList.add('goal-header')
         const title = document.createElement('h5');
         title.innerText = goal.title;
-        entry.appendChild(title);
+        header.appendChild(title);
+
+        //header (icons)
+        const icons = document.createElement('div');
+        const editEntry = document.createElement('i');
+        editEntry.classList.add('flaticon-pencil');
+        icons.appendChild(editEntry);
+
+        const deleteGoal = document.createElement('i');
+        deleteGoal.classList.add('flaticon-delete');
+        icons.appendChild(deleteGoal);
+        header.appendChild(icons);
+
+        entry.appendChild(header);
+
         //Due Date
         const dueDate = document.createElement('p');
         dueDate.innerText = 'Due: ' + goal.due;
         entry.appendChild(dueDate);
+
         //Description
         const description = document.createElement('p');
         description.innerText = goal.description;
